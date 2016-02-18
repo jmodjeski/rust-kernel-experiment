8410b2504626251eaa2b657da45f3a104ee4370d
Joe Modjeski <joemodjeski@FeenixDust.local>
2016-02-17 22:58:22 -0700

    playing around in assembly

--- src/arch/x86_64/boot.asm
+++ src/arch/x86_64/boot.asm
@@ -4,8 +4,69 @@ global start
 
 section .text
 bits 32
+
 start:
-  ; print `Ok` to screen
-  mov dword [0xb8000], 0x2f4b2f4f
+  mov ebx, 0xB8000
+  mov esp, ebx
+  mov byte [esp], 'H'
+  add esp, 1
+  mov byte [esp], 1Fh
+  add esp, 1
+  mov byte [esp], 'e'
+  add esp, 1
+  mov byte [esp], 1Fh
+  add esp, 1
+  mov byte [esp], 'l'
+  add esp, 1
+  mov byte [esp], 1Fh
+  add esp, 1
+  mov byte [esp], 'l'
+  add esp, 1
+  mov byte [esp], 1Fh
+  add esp, 1
+  mov byte [esp], 'o'
+  add esp, 1
+  mov byte [esp], 1Fh
+  add esp, 1
+  mov byte [esp], ' '
+  add esp, 1
+  mov byte [esp], 1Fh
+  add esp, 1
+  mov byte [esp], 'U'
+  add esp, 1
+  mov byte [esp], 1Fh
+  add esp, 1
+  mov byte [esp], 's'
+  add esp, 1
+  mov byte [esp], 1Fh
+  add esp, 1
+  mov byte [esp], 'e'
+  add esp, 1
+  mov byte [esp], 1Fh
+  add esp, 1
+  mov byte [esp], 'r'
+  add esp, 1
+  mov byte [esp], 1Fh
+
+  ; clear remaining area
+clear: 
+  add esp, 1
+  mov byte [esp], 0
+  add esp, 1
+  mov byte [esp], 0
+  mov eax, esp
+  cmp eax, 0xB8EFA
+  jl clear
+
+  ; smiley to signal end of buffer
+  add esp, 1
+  mov byte [esp], ':'
+  add esp, 1
+  mov byte [esp], 1Fh
+  add esp, 1
+  mov byte [esp], ')'
+  add esp, 1
+  mov byte [esp], 1Fh
+
   hlt
   
